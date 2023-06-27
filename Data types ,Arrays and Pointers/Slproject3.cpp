018c12b076d77e2c77832caf518388328c3bea3e
satwik <100366942+sAtM10@users.noreply.github.com>
2023-06-27 19:47:09 +0530

    Add files via upload

---
+++ Data types ,Arrays and Pointers/Slproject3.cpp
@@ -0,0 +1,6 @@
+/*You are working on a ticketing system. A ticket costs $10.
+The office is running a discount campaign: each group of 5 people is getting a discount, 
+which is determined by the age of the youngest person in the group.
+You need to create a program that takes the ages of all 5 people 
+as input and outputs the total price of the tickets.*/
+//project 3 array data type pointer//
