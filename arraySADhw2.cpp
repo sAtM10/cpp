ce97fe0bbf75127e104d52eebc62ff7f49502b30
satwik <100366942+sAtM10@users.noreply.github.com>
2023-07-25 22:52:22 +0530

    Add files via upload

---
+++ arraySADhw2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n[15],i;
+	for(i=0;i<15;i++)
+	{
+		cout<<"enter number"<<endl;
+		cin>>n[i];
+	}
+	for(i=10;i<15;i++)
+	{
+		cout<<"last 5 number is"<<n[i]<<endl;
+	}
+}
+
+	
+	
